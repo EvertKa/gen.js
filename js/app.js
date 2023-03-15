3e8cfd370e9ed994667f58c35bf90b29716398e4
EvertKa <93243148+EvertKa@users.noreply.github.com>
2023-03-15 14:09:28 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// Functions
+
+const clientName = "Kevin";
+
+const clientLastName = undefined;
+
+ 
+
+/* function myKevin(str1 = "Eesnimi", str2 = "Perekonnanimi") {
+
+    console.log(`${str1} ${str2}`)
+
+    return `${str1} ${str2}`;
+
+}
+
+ 
+
+myKevin(clientName, clientLastName);
+ */
+
+// Arrow function
+
+const myKevin = (str1 = "Eesnimi", str2 = "Perekonnanimi") => {
+    console.log(`${str1} ${str2}`)
+    return `${str1} ${str2}`;
+}
+
+myKevin(clientName, clientLastName)
+
+//built-in-functions
+console.log(clientName.charAt(0))
+console.log(clientName.length)
+
