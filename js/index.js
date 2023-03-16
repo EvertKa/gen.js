65e81364b5a2339d9f126f2dd021fe1f15c82520
EvertKa <93243148+EvertKa@users.noreply.github.com>
2023-03-16 11:27:42 +0200

    Delete js directory

--- js/index.js
+++
@@ -1,5 +0,0 @@
-const response = fetch('./dummy.json').then(response => response.json()).then(({ctRoot}) => {
-  const res = ctRoot.filter((person) => person.verified == true)
-  console.log(res)
-})
-
